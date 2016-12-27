9e064a91a8cf638ff82bb5d5c53c15d86036e6e1
kaneko y <kaneko@aluhost>
2016-12-27 17:14:17 +0900

    ZipCloudより得た結果をPostalAddressクラスの配列として返す

--- lib/zipcloud.rb
+++ lib/zipcloud.rb
@@ -1,5 +1,39 @@
 require "zipcloud/version"
+require "net/http"
+require "json"
 
 module Zipcloud
-  # Your code goes here...
+  ZipCloud_URL = "zipcloud.ibsnet.co.jp"
+
+  def self.to_postal_addresses zipcode
+    data = JSON.parse(send_to_zipcloud zipcode)
+
+    data["results"].collect do |dat|
+      PostalAddress.new dat
+    end
+  end
+
+  class PostalAddress
+    attr_reader :state, :city, :town, :state_kana, :city_kana, :town_kana, :zipcode, :prefcode
+    def initialize zipcloud_results
+      @state = zipcloud_results["address1"]
+      @city = zipcloud_results["address2"]
+      @town = zipcloud_results["address3"]
+
+      @state_kana = zipcloud_results["kana1"]
+      @city_kana = zipcloud_results["kana2"]
+      @town_kana = zipcloud_results["kana3"]
+
+      @zipcode = zipcloud_results["zipcode"]
+      @prefcode = zipcloud_results["prefcode"]
+    end
+  end
+
+  def self.send_to_zipcloud zipcode
+    response = Net::HTTP.new(ZipCloud_URL, 80).start do |svr|
+      svr.get("/api/search?" + "zipcode=" + zipcode)
+    end
+
+    response.body
+  end
 end
--- zipcloud.gemspec
+++ zipcloud.gemspec
@@ -6,10 +6,10 @@ require 'zipcloud/version'
 Gem::Specification.new do |spec|
   spec.name          = "zipcloud"
   spec.version       = Zipcloud::VERSION
-  spec.authors       = ["TODO: Write your name"]
-  spec.email         = ["TODO: Write your email address"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.authors       = ["Kaneko Yoshihiro"]
+  spec.email         = ["yoshihiro.kaneko@advantest.com"]
+  spec.summary       = %q{get PostalAddress from Zipcloud}
+  spec.description   = %q{if you want to get PostalAddress, you should input postal code to Zipcloud object.}
   spec.homepage      = ""
   spec.license       = "MIT"
 
